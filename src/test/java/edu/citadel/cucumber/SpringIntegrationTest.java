c9e21ad778846c1b6969d997c8ce812cc029e47f
Chase Bradley <bradleycha@proton.me>
2025-10-10 18:30:33 -0400

    Create boilerplate for cucumber test runner

---
+++ src/test/java/edu/citadel/cucumber/SpringIntegrationTest.java
@@ -0,0 +1,17 @@
+package edu.citadel.main;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class SpringIntegrationTest {
+   public SpringIntegrationTest() {
+      // TODO: implement
+   }
+
+   public void executeGet(String url) {
+      // TODO: implement
+   }
+}
+
